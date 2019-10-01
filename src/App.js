d792ab492c87d9e3682b1e123ca72a121d8e4632
ziveo <bziveo@gmail.com>
2019-10-01 19:39:32 -0400

    Updating App and README.md

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import pdfMake from "pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import "./App.css";
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+const dd = {
+  watermark: {
+    text: "test watermark",
+    opacity: 0.1,
+    bold: true,
+    italics: false
+  },
+  content: [
+    "First paragraph",
+    "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
+    "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
+    "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
+    "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
+    "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
+    "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
+    "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
+  ]
+};
+
+const downloadPDF = () => {
+  pdfMake.createPdf(dd).open();
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span className="App-link" onClick={() => downloadPDF()}>
+          Download PDF
+        </span>
       </header>
     </div>
   );
