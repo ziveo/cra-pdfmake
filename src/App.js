a72d24b62454194855a0438db4e59fb5a743292d
ziveo <bziveo@gmail.com>
2019-10-01 22:59:47 -0400

    Updating example to show PDF in page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import pdfMake from "pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 import "./App.css";
@@ -20,24 +20,46 @@ const dd = {
     "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
     "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
     "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
-    "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
+    "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines"
   ]
 };
 
-const downloadPDF = () => {
-  pdfMake.createPdf(dd).open();
-};
+class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <span className="App-link" onClick={() => downloadPDF()}>
-          Download PDF
-        </span>
-      </header>
-    </div>
-  );
+    this.iframeRef = React.createRef();
+    this.state = {
+      pdfUrl: null
+    };
+  }
+
+  downloadPDF = () => {
+    console.log(this.iframeRef);
+    const pdfDocGenerator = pdfMake.createPdf(dd);
+    pdfDocGenerator.getDataUrl(dataUrl => {
+      this.setState({ pdfUrl: dataUrl });
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <span className="App-link" onClick={() => this.downloadPDF()}>
+            Show PDF
+          </span>
+          <iframe
+            title="pdf-iframe"
+            src={this.state.pdfUrl}
+            frameBorder="0"
+            ref={this.iframeRef}
+            style={{ width: 400, height: 650, backgroundColor: "#ccc" }}
+          />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
