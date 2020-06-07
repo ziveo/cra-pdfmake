6306e41cfba936287518d834913a5e46ec8a51df
ziveo <bzivkovic@blueraster.com>
2020-06-07 15:04:50 -0400

    Adding `prettier`

--- src/App.js
+++ src/App.js
@@ -1,67 +1,62 @@
-import React, { useState } from "react";
-import { ControlledEditor } from "@monaco-editor/react";
-import pdfMake from "pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
+import React, { useState } from 'react';
+import { ControlledEditor } from '@monaco-editor/react';
+import pdfMake from 'pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 
-import "./App.css";
+import './App.css';
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const initialPdfCode = {
   watermark: {
-    text: "test watermark",
+    text: 'test watermark',
     opacity: 0.05,
     bold: true,
-    italics: false
+    italics: false,
   },
   info: {
-    title: "PDF Document",
-    author: "john doe",
-    subject: "subject of document",
-    keywords: "keywords for document"
+    title: 'PDF Document',
+    author: 'john doe',
+    subject: 'subject of document',
+    keywords: 'keywords for document',
   },
   content: [
-    "First paragraph 123",
-    "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines"
-  ]
+    'First paragraph 123',
+    'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines',
+  ],
 };
 
 function App() {
   const [code, setCode] = useState(JSON.stringify(initialPdfCode, null, 2));
   const [pdfUrl, setPdfUrl] = useState(() => {
     const pdfDocGenerator = pdfMake.createPdf(initialPdfCode);
-    pdfDocGenerator.getDataUrl(dataUrl => {
+    pdfDocGenerator.getDataUrl((dataUrl) => {
       setPdfUrl(dataUrl);
     });
   });
 
-  const createPDF = pdfCode => {
+  const createPDF = (pdfCode) => {
     const pdfDocGenerator = pdfMake.createPdf(JSON.parse(pdfCode));
-    pdfDocGenerator.getDataUrl(dataUrl => {
+    pdfDocGenerator.getDataUrl((dataUrl) => {
       setPdfUrl(dataUrl);
     });
   };
 
   return (
-    <div className="App">
-      <div className="App-content">
-        <div id="CodeContainer">
+    <div className='App'>
+      <div className='App-content'>
+        <div id='CodeContainer'>
           <ControlledEditor
-            height="90vh"
+            height='90vh'
             value={code}
-            language="json"
+            language='json'
             onChange={(ev, value) => {
               createPDF(value);
             }}
           />
         </div>
-        <div id="PdfContainer">
-          <iframe
-            id="PdfContainer__iframe"
-            title="pdf-iframe"
-            src={pdfUrl}
-            frameBorder={0}
-          />
+        <div id='PdfContainer'>
+          <iframe id='PdfContainer__iframe' title='pdf-iframe' src={pdfUrl} frameBorder={0} />
         </div>
       </div>
     </div>
