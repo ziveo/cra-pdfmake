307153fa473fcf4a2f012b72537425c26b738f50
ziveo <bzivkovic@blueraster.com>
2020-06-12 22:21:20 -0400

    Make PWA during build step

--- src/index.js
+++ src/index.js
@@ -12,4 +12,8 @@ ReactDOM.render(<App />, rootElement);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (process.env.NODE_ENV !== 'development') {
+  serviceWorker.unregister();
+} else {
+  serviceWorker.register();
+}
