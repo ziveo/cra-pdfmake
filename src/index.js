56c229ecafdb422f2ab44d1053caef991dc744d4
ziveo <bzivkovic@blueraster.com>
2020-06-12 22:31:08 -0400

    PWA register

--- src/index.js
+++ src/index.js
@@ -12,8 +12,4 @@ ReactDOM.render(<App />, rootElement);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-if (process.env.NODE_ENV !== 'development') {
-  serviceWorker.unregister();
-} else {
-  serviceWorker.register();
-}
+serviceWorker.register();
