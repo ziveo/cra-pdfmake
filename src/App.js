127d7d63f86b82646460e95c08667c8122dd52ba
ziveo <bzivkovic@blueraster.com>
2020-06-13 21:41:44 -0400

    Moving H1 to main component and adding version tracking

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import loadable from '@loadable/component';
 import ReactGA from 'react-ga';
 import { initialPdfCode } from './app.config';
 
+import packageJson from '../package.json';
+
 import './App.scss';
 import pdfUrl from './pdf/initial-pdf.pdf';
 
@@ -24,31 +26,37 @@ function App() {
   };
 
   return (
-    <div className='App'>
-      <div id='CodeContainer'>
-        <ControlledEditor
-          value={code}
-          language='json'
-          options={{
-            wordWrap: 'on',
-            minimap: {
-              enabled: false,
-            },
-            formatOnPaste: true,
-            formatOnType: true,
-          }}
-          onChange={createPDF}
-        />
-      </div>
+    <>
+      <header>
+        <h1 id='App__title'>PDF Maker</h1>
+        <sub id='App__version'>v{packageJson.version}</sub>
+      </header>
+      <div className='App'>
+        <div id='CodeContainer'>
+          <ControlledEditor
+            value={code}
+            language='json'
+            options={{
+              wordWrap: 'on',
+              minimap: {
+                enabled: false,
+              },
+              formatOnPaste: true,
+              formatOnType: true,
+            }}
+            onChange={createPDF}
+          />
+        </div>
 
-      <div id='PdfContainer'>
-        {!pdfEdited ? (
-          <iframe id='PdfContainer__iframe' title='pdf-iframe' src={pdfUrl} frameBorder={0} />
-        ) : (
-          <PdfContainer code={code} />
-        )}
+        <div id='PdfContainer'>
+          {!pdfEdited ? (
+            <iframe id='PdfContainer__iframe' title='pdf-iframe' src={pdfUrl} frameBorder={0} />
+          ) : (
+            <PdfContainer code={code} />
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
